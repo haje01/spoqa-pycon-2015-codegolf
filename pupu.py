e85539a393e9b744477e8274f2a11ad98a5e237f
JeongJu Kim <haje01@gmail.com>
2015-08-30 12:11:49 +0900

    minimize

--- pupu.py
+++ pupu.py
@@ -1,19 +1,5 @@
-from __future__ import print_function
-
-D = ''' T( S+ R, Q. Q.%$ Q-", Q: R: C)): A++7 >.,6 >.+5 89%(#+ 4G 1I /M .O -Q ,R +T +U *.$0$4 *+$%#*#%#2 *+")"'#)"1 +)"*"'"*#0 +)"+"&"+"0 +)"+"&"+"0 )+"+"&"+"2 (,"*"'"+"4 '.")"(#(#5 &1',(7 %` %` %` %` &^ '\ ([ )Y *V -Q 1H '''
-
-
-def draw_pupu():
-    i = 0
-    e = ord('!')
-    while i < len(D):
-        if D[i] == ' ':
-            print('')
-            i += 1
-        else:
-            o, l = ord(D[i]) - e, ord(D[i+1]) - e
-            print('{}{}'.format(' ' * o, '*' * l), end='')
-            i += 2
-
-if __name__ == '__main__':
-    draw_pupu()
+from sys import stdout as s
+x,i,D=ord('!'),0,''' T( S+ R, Q. Q.%$ Q-", Q: R: C)): A++7 >.,6 >.+5 89%(#+ 4G 1I /M .O -Q ,R +T +U *.$0$4 *+$%#*#%#2 *+")"'#)"1 +)"*"'"*#0 +)"+"&"+"0 +)"+"&"+"0 )+"+"&"+"2 (,"*"'"+"4 '.")"(#(#5 &1',(7 %` %` %` %` &^ '\ ([ )Y *V -Q 1H '''
+while i<len(D):
+ if D[i]==' ':s.write('\n');i+=1
+ else:o,l=ord(D[i])-x,ord(D[i+1])-x;s.write('{}{}'.format(' '*o,'*'*l));i+=2
